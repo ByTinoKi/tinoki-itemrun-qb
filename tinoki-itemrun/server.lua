f6215ee2bc1bf8df2000ac20fa5fdb2ebc62becc
ByTinoKi <dfatinoki@gmail.com>
2022-11-20 11:44:09 +0100

    Server side error fix for qbcore

--- tinoki-itemrun/server.lua
+++ tinoki-itemrun/server.lua
@@ -7,7 +7,7 @@ AddEventHandler('tinoki-items:collect', function()
 	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
 	
-	for k,v in ipairs(Config.tinoki-items)do
+	for k,v in ipairs(Config.Items)do
 		if Config.RandomAmount then 
 			local rAmount = math.random(Config.MinAmount, Config.MaxAmount)
 			Player.Functions.AddItem(v.item, rAmount)
